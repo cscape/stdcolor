cf468a497d1efa2ecfd98d40fdd5b55b6bbe8aea
ciolt <cioltdev@outlook.com>
2019-02-22 23:04:54 -0500

    update example code

--- example/index.js
+++ example/index.js
@@ -1,19 +1,14 @@
 // Dependencies
-var FlatColors = require("../lib")
+var StdColor = require('../lib')
 
 // Get the flat red
-console.log(FlatColors(255, 0, 0));
+console.log(StdColor(255, 0, 0))
 // => [211, 84, 0]
 
 // Same thing, but using a rgb array
-console.log(FlatColors([255, 0, 0]));
+console.log(StdColor([255, 0, 0]))
 // => [211, 84, 0]
-
 
 // Still same color but using hex color
-console.log(FlatColors("#f00"));
+console.log(StdColor('#f00'))
 // => [211, 84, 0]
-
-// Random flat color
-console.log(FlatColors());
-// => [?, ?, ?]
